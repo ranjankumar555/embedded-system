3dd082c5e091ee91d2a05a1610cc9f3b74da2e12
ranjankumar555 <vtu16964@veltech.edu.in>
2024-12-21 03:42:30 +0530

    compilation process of c

---
+++ c/compilation-stage/filename.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int i = 5;      // variable of type 'int'
+	char ch = 'A';  // variable of type 'char'
+	float f = 23.5f; // variable of type 'float'
+
+	// printing the data
+	printf("i = %d\n", i);
+	printf("ch = %c\n",ch);
+	printf("f = %f\n", f);
+
+	return 0;
+}
