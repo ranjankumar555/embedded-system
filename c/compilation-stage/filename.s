4665353f59a347f0a86942e8003a617690e840f6
ranjankumar555 <vtu16964@veltech.edu.in>
2024-12-31 01:08:27 +0530

    compilation stages

---
+++ c/compilation-stage/filename.s
@@ -0,0 +1,75 @@
+	.file	"filename.c"
+	.text
+	.section	.rodata
+.LC1:
+	.string	"i = %d\n"
+.LC2:
+	.string	"ch = %c\n"
+.LC3:
+	.string	"f = %f\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$5, -8(%rbp)
+	movb	$65, -9(%rbp)
+	movss	.LC0(%rip), %xmm0
+	movss	%xmm0, -4(%rbp)
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movsbl	-9(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	pxor	%xmm1, %xmm1
+	cvtss2sd	-4(%rbp), %xmm1
+	movq	%xmm1, %rax
+	movq	%rax, %xmm0
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdi
+	movl	$1, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.rodata
+	.align 4
+.LC0:
+	.long	1102839808
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
