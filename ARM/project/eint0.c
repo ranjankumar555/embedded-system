503b227d2e7696feaa5ead1001a4aa6a10f61bbe
Ranjan Kumar <111798933+ranjankumar555@users.noreply.github.com>
2025-05-13 18:59:11 +0530

    Add files via upload

---
+++ ARM/project/eint0.c
@@ -0,0 +1,28 @@
+#include<lpc21xx.h>
+#include"headerb.h"
+extern u32 flag;
+void EINT0_Handler(void)__irq
+{
+			    can1 m;
+				m.id=0x50;
+				m.dlc=8;
+				m.rtr=1;
+				can1_tx(m);
+				flag^=1;
+        EXTINT=1;
+        VICVectAddr=0;
+}
+void config_VIC_for_EINT0(void)
+{
+        VICIntSelect|=0;
+        VICVectCntl1=14|(1<<5);
+        VICVectAddr1=(u32)EINT0_Handler;
+        VICIntEnable|=(1<<14);
+}
+void config_EINT0(void)
+{
+        PINSEL1|=1;
+        EXTMODE=1;
+        EXTPOLAR=0;
+}
+
