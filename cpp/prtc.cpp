3e2fd683113942d177b1b17c7f9c347b6865dd0a
ranjankumar555 <vtu16964@veltech.edu.in>
2025-05-19 16:56:29 +0530

    updated

--- cpp/prtc.cpp
+++ cpp/prtc.cpp
@@ -1,10 +1,8 @@
 #include<iostream>
-#include<cstring>
 using namespace std;
-
 int main(){
-	string s1;
-	cin>>s1;
-	cout<<s1<<endl;
-	return 0;
+	string s2 = "bangalore";
+	string s = "hello" + " " + s2 + "again" + '!';
+	cout<<s<<endl;
 }
+
